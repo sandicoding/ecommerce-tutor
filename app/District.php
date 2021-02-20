35e78bc93d05a2faf5282478eda7fa40ef9b2f0d
sandi pratama <sandicoding@gmail.com>
2021-02-20 21:56:07 +0700

    complate transaction customers

--- app/District.php
+++ app/District.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class District extends Model
 {
     //
+    //BUAT RELASI KE MODEL CITY.PHP
+    public function city()
+    {
+        return $this->belongsTo(City::class);
+    }
 }
--- app/Http/Controllers/Ecommerce/CartController.php
+++ app/Http/Controllers/Ecommerce/CartController.php
@@ -5,6 +5,14 @@ namespace App\Http\Controllers\Ecommerce;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Product;
+use App\Province;
+use App\City;
+use App\District;
+use App\Customer;
+use App\Order;
+use App\OrderDetail;
+use Illuminate\Support\Str;
+use DB;
 
 class CartController extends Controller
 {
@@ -17,7 +25,7 @@ class CartController extends Controller
         ]);
 
         //AMBIL DATA CART DARI COOKIE, KARENA BENTUKNYA JSON MAKA KITA GUNAKAN JSON_DECODE UNTUK MENGUBAHNYA MENJADI ARRAY
-        $carts = json_decode($request->cookie('dw-carts'), true); 
+        $carts = $this->getCarts();
     
         //CEK JIKA CARTS TIDAK NULL DAN PRODUCT_ID ADA DIDALAM ARRAY CARTS
         if ($carts && array_key_exists($request->product_id, $carts)) {
@@ -46,7 +54,7 @@ class CartController extends Controller
     public function listCart()
     {
         //MENGAMBIL DATA DARI COOKIE
-        $carts = json_decode(request()->cookie('dw-carts'), true);
+        $carts = $this->getCarts();
         //UBAH ARRAY MENJADI COLLECTION, KEMUDIAN GUNAKAN METHOD SUM UNTUK MENGHITUNG SUBTOTAL
         $subtotal = collect($carts)->sum(function($q) {
             return $q['qty'] * $q['product_price']; //SUBTOTAL TERDIRI DARI QTY * PRICE
@@ -55,10 +63,19 @@ class CartController extends Controller
         return view('ecommerce.cart', compact('carts', 'subtotal'));
     }
 
+    private function getCarts()
+    {
+        $carts = json_decode(request()->cookie('dw-carts'), true);
+        $carts = $carts != '' ? $carts:[];
+        return $carts;
+    }
+
+
+
     public function updateCart(Request $request)
     {
         //AMBIL DATA DARI COOKIE
-        $carts = json_decode(request()->cookie('dw-carts'), true);
+        $carts = $this->getCarts();
         //KEMUDIAN LOOPING DATA PRODUCT_ID, KARENA NAMENYA ARRAY PADA VIEW SEBELUMNYA
         //MAKA DATA YANG DITERIMA ADALAH ARRAY SEHINGGA BISA DI-LOOPING
         foreach ($request->product_id as $key => $row) {
@@ -75,5 +92,128 @@ class CartController extends Controller
         $cookie = cookie('dw-carts', json_encode($carts), 2880);
         //DAN STORE KE BROWSER.
         return redirect()->back()->cookie($cookie);
+    }
+
+    public function checkout() {
+
+        //query untuk ambil data semua provinsi
+        $provinces = Province::orderBy('created_at', 'DESC')->get();
+        $carts = $this->getCarts();
+
+        //Menghitung subtotal dari keranjang belanja
+        $subtotal = collect($carts)->sum(function($q) {
+            return $q['qty'] * $q['product_price'];
+        });
+
+        //ME-LOAD VIEW CHECKOUT.BLADE.PHP DAN PASSING DATA PROVINCES, CARTS DAN SUBTOTAL
+        return view('ecommerce.checkout', compact('provinces', 'carts', 'subtotal'));
+
+    }
+
+    public function getCity()
+    {
+        //QUERY UNTUK MENGAMBIL DATA KOTA / KABUPATEN BERDASARKAN PROVINCE_ID
+        $cities = City::where('province_id', request()->province_id)->get();
+        //KEMBALIKAN DATANYA DALAM BENTUK JSON
+        return response()->json(['status' => 'success', 'data' => $cities]);
+    }
+
+    public function getDistrict()
+    {
+        //QUERY UNTUK MENGAMBIL DATA KECAMATAN BERDASARKAN CITY_ID
+        $districts = District::where('city_id', request()->city_id)->get();
+        //KEMUDIAN KEMBALIKAN DATANYA DALAM BENTUK JSON
+        return response()->json(['status' => 'success', 'data' => $districts]);
+    }
+
+    public function processCheckout(Request $request)
+    {
+        //VALIDASI DATANYA
+        $this->validate($request, [
+            'customer_name' => 'required|string|max:100',
+            'customer_phone' => 'required',
+            'email' => 'required|email',
+            'customer_address' => 'required|string',
+            'province_id' => 'required|exists:provinces,id',
+            'city_id' => 'required|exists:cities,id',
+            'district_id' => 'required|exists:districts,id'
+        ]);
+
+        //INISIASI DATABASE TRANSACTION
+        //DATABASE TRANSACTION BERFUNGSI UNTUK MEMASTIKAN SEMUA PROSES SUKSES UNTUK KEMUDIAN DI COMMIT AGAR DATA BENAR BENAR DISIMPAN, JIKA TERJADI ERROR MAKA KITA ROLLBACK AGAR DATANYA SELARAS
+        DB::beginTransaction();
+        try {
+            //CHECK DATA CUSTOMER BERDASARKAN EMAIL
+            $customer = Customer::where('email', $request->email)->first();
+            //JIKA DIA TIDAK LOGIN DAN DATA CUSTOMERNYA ADA
+            if (!auth()->check() && $customer) {
+                //MAKA REDIRECT DAN TAMPILKAN INSTRUKSI UNTUK LOGIN 
+                return redirect()->back()->with(['error' => 'Silahkan Login Terlebih Dahulu']);
+            }
+
+            //AMBIL DATA KERANJANG
+            $carts = $this->getCarts();
+            //HITUNG SUBTOTAL BELANJAAN
+            $subtotal = collect($carts)->sum(function($q) {
+                return $q['qty'] * $q['product_price'];
+            });
+
+            //SIMPAN DATA CUSTOMER BARU
+            $customer = Customer::create([
+                'name' => $request->customer_name,
+                'email' => $request->email,
+                'phone_number' => $request->customer_phone,
+                'address' => $request->customer_address,
+                'district_id' => $request->district_id,
+                'status' => false
+            ]);
+
+            //SIMPAN DATA ORDER
+            $order = Order::create([
+                'invoice' => Str::random(4) . '-' . time(), //INVOICENYA KITA BUAT DARI STRING RANDOM DAN WAKTU
+                'customer_id' => $customer->id,
+                'customer_name' => $customer->name,
+                'customer_phone' => $request->customer_phone,
+                'customer_address' => $request->customer_address,
+                'district_id' => $request->district_id,
+                'subtotal' => $subtotal
+            ]);
+
+            //LOOPING DATA DI CARTS
+            foreach ($carts as $row) {
+                //AMBIL DATA PRODUK BERDASARKAN PRODUCT_ID
+                $product = Product::find($row['product_id']);
+                //SIMPAN DETAIL ORDER
+                OrderDetail::create([
+                    'order_id' => $order->id,
+                    'product_id' => $row['product_id'],
+                    'price' => $row['product_price'],
+                    'qty' => $row['qty'],
+                    'weight' => $product->weight
+                ]);
+            }
+            
+            //TIDAK TERJADI ERROR, MAKA COMMIT DATANYA UNTUK MENINFORMASIKAN BAHWA DATA SUDAH FIX UNTUK DISIMPAN
+            DB::commit();
+
+            $carts = [];
+            //KOSONGKAN DATA KERANJANG DI COOKIE
+            $cookie = cookie('dw-carts', json_encode($carts), 2880);
+            //REDIRECT KE HALAMAN FINISH TRANSAKSI
+            return redirect(route('front.finish_checkout', $order->invoice))->cookie($cookie);
+        } catch (\Exception $e) {
+            //JIKA TERJADI ERROR, MAKA ROLLBACK DATANYA
+            DB::rollback();
+            //DAN KEMBALI KE FORM TRANSAKSI SERTA MENAMPILKAN ERROR
+            return redirect()->back()->with(['error' => $e->getMessage()]);
+        }
+    }
+
+    public function checkoutFinish($invoice)
+    {
+        //AMBIL DATA PESANAN BERDASARKAN INVOICE
+        $order = Order::with(['district.city'])->where('invoice', $invoice)->first();
+        //LOAD VIEW checkout_finish.blade.php DAN PASSING DATA ORDER
+        return view('ecommerce.checkout_finish', compact('order'));
     }
 }
